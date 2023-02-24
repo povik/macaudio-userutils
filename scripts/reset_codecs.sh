886a88a617e097992a117c78035348afb3a43bc9
Martin Povišer <povik+lin@cutebit.org>
2023-02-24 13:59:40 +0100

    scripts: Reset headphone codecs too
    Signed-off-by: Martin Povišer <povik+lin@cutebit.org>

--- scripts/reset_codecs.sh
+++ scripts/reset_codecs.sh
@@ -10,6 +10,12 @@ get_driver() {
 			ti,sn012776)
 				driver="/sys/bus/i2c/drivers/tas2764"
 				;;
+			cirrus,cs42l83)
+				driver="/sys/bus/i2c/drivers/cs42l83"
+				;;
+			cirrus,cs42l84)
+				driver="/sys/bus/i2c/drivers/cs42l84"
+				;;
 		esac
 	done < $candidate/of_node/compatible
 }
@@ -18,7 +24,7 @@ per_speaker_codec() {
 	for candidate in /sys/devices/platform/soc/*.i2c/i2c-*/?-????; do
 		while IFS= read -r -d '' compatible ; do
 			case "$compatible" in
-				ti,tas5770l|ti,sn012776)
+				ti,tas5770l|ti,sn012776|cirrus,cs42l83|cirrus,cs42l84)
 					if test -f "$candidate/of_node/sound-name-prefix"; then
 						IFS= read -r -d '' name \
 							< $candidate/of_node/sound-name-prefix
